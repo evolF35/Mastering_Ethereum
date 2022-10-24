44b6b6d47982dd6694fac88a0cab7d0fdc2c12c5
evolF35 <maccyair@gmail.com>
2022-10-24 07:15:16 -0700

    inheritance

--- faucet.sol
+++ faucet.sol
@@ -2,12 +2,36 @@
 
 pragma solidity 0.8.17;
 
-contract Faucet {
+contract Owned{
+    address owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+
+    modifier onlyOwner {
+        require(msg.sender == owner,"only owner");
+        _;
+    }
+}
+
+
+contract Mortal is Owned {
+
+function destroy() public onlyOwner{
+            selfdestruct(payable(owner));
+}
+
+}
+
+contract Faucet is Mortal {
+
     receive() external payable {}
 
     function withdraw(uint amount) public {
-        require(amount <= 100000000000000000);
-
+        require(amount <= .1 ether);
+        require(address(this).balance >= amount, "insufficient" );
     payable(msg.sender).transfer(amount);
 
     }
